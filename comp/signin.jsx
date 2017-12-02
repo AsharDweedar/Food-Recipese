dd4508640e287d3b1d05680b4459a05d90f9435d
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-12-02 16:25:34 +0200

    adding required components files

--- comp/signin.jsx
+++ comp/signin.jsx
@@ -6,29 +6,23 @@ class Layout extends React.Component {
     super(props);
     this.state = {
       showSignUp : false,
-      showSignIn : true,
+      showSignIn : false,
       showRecipes : false,
       showRecipesPage : false,
       showRecomendations : false,
       current : "showRecipesPage",
     }
+    // debugger;
   }
   toShow (show) {
     //conditional rendering
-    this.state[show] = true; //the passed value is now true => will be shown 
     this.state[current] = false; //change the currently shown tab to false => will be hided
-    this.setState({ current: show }); //the passed value is now the cuurrent
+    this.state.current = show; //the passed value is now the cuurrent
+    this.state[show] = true; //the passed value is now true => will be shown 
   }
   render() {
     return <div>
-      
-        { this.state.showSignUp && <signup /> }
-        { this.state.showSignIn && <signin /> }
-        { this.state.showRecipes && <recipes /> }
-        { this.state.showRecipesPage && <recipepage /> }
-        { this.state.showRecomendations && <recomendations /> }
-        Hello world, this is <window.something
-        >
+        Hello world, this is <window.something>
           inside something
         </window.something>
       </div>;
