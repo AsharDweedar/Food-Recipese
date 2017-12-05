8e02ccfb1b6daf97ae79b2eb6a9dd889a74a7974
AsharDweedar <Ashar.Dweedar@gmail.com>
2017-12-05 10:34:36 +0200

    last push

--- comp/recipepage.jsx
+++ comp/recipepage.jsx
@@ -4,17 +4,30 @@ class recipepage extends React.Component {
     console.log("props recipepage : ");
     console.log(props);
     super(props);
-    this.state = { ...props.recipe , user : props.user }
+    this.state = { user : props.user }
+    var that = this;
+    firebase
+      .database()
+      .ref("recipese/" + props.recipe)
+      .once('value')
+      .then((rec) => {
+        console.log(that)
+        that.setState({recipe : rec.val()});
+        console.log(that.state)
+      })
+      .catch(({ message }) => {
+        console.log(message);
+      });
   }
   subcomment () {
-    var st = this.state;
-    var comm = $("#comm").val();
-    var displayName = st.user.displayName;
-    var co = st.comments.concat({ auth: displayName, comm: comm });
+    let st = this.state;
+    let comm = $("#comm").val();
+    let displayName = st.user.displayName;
+    let co = st.recipe.comments.concat({ auth: displayName, comm: comm });
     console.log(co)
     firebase
       .database()
-      .ref("recipese/" + st.id)
+      .ref("recipese/" + st.recipe.id)
       .update({ comments : co })
       .catch(({ message }) => {
         console.log(message);
@@ -22,39 +35,39 @@ class recipepage extends React.Component {
   }
     
   addfav() {
-    var st = this.state;
-    st.user.fav.push(st.recipe.name);
-    firebase
-      .database()
-      .ref("users/" + this.state.user.id)
-      .update(st.user)
-      .catch(({ message }) => {
-        console.log(message);
-      });
+    // var st = this.state;
+    // st.user.fav.push(st.recipe.name);
+    // firebase
+    //   .database()
+    //   .ref("users/" + this.state.user.id)
+    //   .update(st.user)
+    //   .catch(({ message }) => {
+    //     console.log(message);
+    //   });
   }
 
   render() {
-    if (!this.props.recipe) {
+    if (this.props.recipe && typeof this.props.recipe === "string") {
       return <div className="alert alert-warning" role="alert">
           no recipe selected !!
         </div>;
     }
     var ingrediantsList = [];
-    for (var ingrediant of this.props.recipe.ingrediants) {
+    for (var ingrediant of this.state.recipe.ingrediants) {
       ingrediantsList.push(<li className="list-group-item">
         <span className="badge">{ingrediant.quantity}</span>
         {ingrediant.ingrediant}
       </li>);
     }
-    var comments = [];
-    let comms = this.props.recipe.comments;
-    for (var comm in comms) {
-      comments.push(<div className="list-group">
+    var comms = [];
+    let DBcomms = this.state.recipe.comments;
+    for (var comm in DBcomms) {
+      comms.push(<div className="list-group">
           <a href="#" className="list-group-item disabled">
-            {comms[comm].auth}
+            {DBcomms[comm].auth}
           </a>
           <a href="#" className="list-group-item">
-            {comms[comm].comm}
+            {DBcomms[comm].comm}
           </a>
         </div>);
     }
@@ -62,12 +75,12 @@ class recipepage extends React.Component {
         <div className="col-sm-6 col-md-4">
           <div className="thumbnail">
             <div className="caption">
-              <h3>{this.props.recipe.name}</h3>
+              <h3>{this.state.recipe.name}</h3>
               <ul className="list-group">{ingrediantsList}</ul>
-              <h3>{this.props.recipe.time}</h3>
-              <p>{this.props.recipe.preparing}</p>
+              <h3>{this.state.recipe.time}</h3>
+              <p>{this.state.recipe.preparing}</p>
               <hr />
-              {comments}
+              {comms}
               {this.props.isLoggedIn && <div>
                   <input id="comm" type="text" className="form-control" placeholder="add comment" />
                   <p />
--- comp/recipes.jsx
+++ comp/recipes.jsx
@@ -11,15 +11,15 @@ class recipes extends React.Component {
     var recipes = this.props.recipes;
     for (var i = 0; i < recipes.length; i++) {
       var recipe = recipes[i];
-      list.push(<div className="row" width = "40%"onClick={() => this.props.show("showRecipePage", recipe)} key={i} >
-        <div className="col-sm-6 col-md-4">
-          <div className="thumbnail">
-            <div className="caption">
-              <h3>{recipe.name}</h3>
+      list.push(<div className="row" width="40%" onClick={() => this.props.show("showRecipePage", recipe.id)} key={recipe.id}>
+          <div className="col-sm-6 col-md-4">
+            <div className="thumbnail">
+              <div className="caption">
+                <h3>{recipe.name}</h3>
+              </div>
             </div>
           </div>
-        </div>
-      </div>);
+        </div>);
     }
     return <div>
       {list}
